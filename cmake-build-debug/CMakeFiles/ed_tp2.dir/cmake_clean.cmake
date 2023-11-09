64b8af11fc51f86636ac74447e430f1f9ddbf286
Greg <gregory.vs123@gmail.com>
2023-11-09 10:52:40 -0300

    add clion + debug config

---
+++ cmake-build-debug/CMakeFiles/ed_tp2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ed_tp2.dir/src/main.cpp.o"
+  "CMakeFiles/ed_tp2.dir/src/main.cpp.o.d"
+  "ed_tp2"
+  "ed_tp2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ed_tp2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
